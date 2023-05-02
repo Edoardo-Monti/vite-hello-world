99d76b38ca1a21543921af846865316532dbc43f
Your Namede <edoardo.monti.bo@gmail.com>
2023-05-02 16:38:07 +0200

    fine

---
+++ src/components/AppHero.vue
@@ -0,0 +1,37 @@
+<template>
+    <div >
+        <div class="box">
+            <img src="../assets/scott-estrada-developer.jpg" alt="">
+            <p id="testo">Guarda come sono felice</p>
+        </div>
+            
+            
+        
+    </div>
+    
+</template>
+
+<script>
+
+    export default{
+        name: "AppHero",
+
+    }
+</script>
+
+<style scoped>
+
+.box {
+  position: relative;   
+}
+
+#testo{
+  position: absolute;
+  z-index: 1;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: black;
+}
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>La mia prima app con Vite!</h1>  
+    </div>
+    
+</template>
+
+<script>
+
+    export default{
+        name: "AppTitle",
+    }
+</script>
+
+<style>
+
+</style>
